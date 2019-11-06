96af167fe0a8290493488a543ae0326832f66a40
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2019-11-06 00:39:49 -0800

    Make lint happy
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ export TITLE="Homekit Alsa Speaker"
 export DESCRIPTION="Control your alsa devices volume with HomeKit"
 export IMAGE_NAME="homekit-alsa"
 
-# shellcheck source=vars.sh
+# shellcheck source=/dev/null
 . "$(cd "$(dirname "${BASH_SOURCE[0]:-$PWD}")" 2>/dev/null 1>&2 && pwd)/helpers.sh"
